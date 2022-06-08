af9b384c3317749ab68cb4169ae84e9b058cce1f
MubarekSD <mubareksd@gmail.com>
2022-06-08 07:58:07 +0300

    done all the work

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int add(int a, int b)
+{
+int c;
+c = a + b;
+return (c);
+}
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int _abs(int n)
+{
+
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	return (n);
+}
