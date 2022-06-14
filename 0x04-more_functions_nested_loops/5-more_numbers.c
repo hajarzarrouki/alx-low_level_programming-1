474112ef2904f0e00e717242f177f4ca4ca3792a
MubarekSD <mubareksd@gmail.com>
2022-06-14 08:49:35 +0300

    made some changes

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,11 +4,16 @@
  */
 void more_numbers(void)
 {
+int i, j;
 for (int i = 0; i <= 9; i++)
 {
-for (int j = 48; j <= 62; j++)
+for (int j = 0; j <= 14; j++)
 {
-_putchar(j);
+if (j >= 10)
+{
+_putchar((j / 10) + 48);
+}
+_putchar((j % 10) + 48);
 }
 _putchar('\n');
 }
