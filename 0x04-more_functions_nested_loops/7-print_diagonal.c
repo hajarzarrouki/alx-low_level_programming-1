cbeeca2e4b2e5d8f6cdf67299deef89589efc408
MubarekSD <mubareksd@gmail.com>
2022-06-15 10:52:16 +0300

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,18 +1,33 @@
 #include "main.h"
+
 /**
- * print_diagonal - prints a diagonal line
- * @n: number of times to print '\'
+ * print_diagonal - Entry point
+ *@n: number of times to print '\' in diagonal
+ * Return: no return
  */
 void print_diagonal(int n)
 {
-int i, j;
-for (i = 0; i < n; i++)
-{
-for (j = 0; j < i; j++)
-{
-_putchar(' ');
-}
-_putchar('\\');
-_putchar('\n');
-}
+	int i;
+	char k;
+	char s;
+	int j;
+
+	k = '\\';
+	s = ' ';
+	for (i = 0; i < n; i++)
+	{
+		if (n > 1)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(s);
+			}
+			_putchar(k);
+			_putchar('\n');
+		}
+	}
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
 }
