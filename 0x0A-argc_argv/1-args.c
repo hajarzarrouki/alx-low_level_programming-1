5f15eb87b62eda8cc7eb95f1ba8de369f07ff403
MubarekSD <mubareksd@gmail.com>
2022-06-24 13:41:36 +0300

    implemented betty coding style

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
  * main - prints the number of arguments that are passed to it
- * @argc:
- * @argv:
+ * @argc: input
+ * @argv: input
  * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
