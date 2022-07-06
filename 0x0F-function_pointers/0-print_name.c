d58aee10bd7a9af8d7ed38a43752b43b3c648cbb
MubarekSD <mubareksd@gmail.com>
2022-07-06 15:57:43 +0300

    modify

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include "function_pointers.h"
 /**
- * print_name - Main Entry
+ * print_name - prints a name as is
  * @name: input
  * @f: input
- */
+**/
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
