f203e779ef0f19704dae03710234692d613ed826
Mubarek Seid <mubareksd@gmail.com>
2022-07-29 20:35:13 +0300

    fixed betty coding style issue

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,16 +6,16 @@
 
 void print_alphabet_x10(void)
 {
-		int i;
-		char c;
+	int i;
+	char c;
 
-		for (i = 0; i <= 9; i++)
+	for (i = 0; i <= 9; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
 		{
-				for (c = 'a'; c <= 'z'; c++)
-				{
-						_putchar(c);
-				}
-
-				_putchar('\n');
+			_putchar(c);
 		}
+
+		_putchar('\n');
+	}
 }
