b54d2e30a740dfbe19fb2bad2b1c71a5a2c43e15
Mubarek Seid <mubareksd@gmail.com>
2022-07-29 23:43:37 +0300

    12. The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,48 +1,48 @@
 #include "main.h"
+
 /**
- * print_times_table - Entry point
+ * print_times_table - prints the n times table, starting with 0.
  * @n: number of the times table
- */
+**/
+
 void print_times_table(int n)
 {
-int i, j, k;
-if (n >= 0 && n <= 15)
-{
-for (i = 0; i <= n; i++)
-{
-for (j = 0; j <= n; j++)
-{
-k = j * i;
-if (j == 0)
-{
-_putchar(k + '0');
-}
-else if (k < 10 && j != 0)
-{
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar(' ');
-_putchar(k + '0');
-}
-else if (k >= 10 && k < 100)
-{
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar((k / 10) + '0');
-_putchar((k % 10) + '0');
-}
-else if (k >= 100)
-{
-_putchar(',');
-_putchar(' ');
-_putchar((k / 100) + '0');
-_putchar(((k / 10) % 10) + '0');
-_putchar((k % 10) + '0');
-}
-}
-_putchar('\n');
-}
-}
+	int i, j, k, l;
+
+	if (n < 15 && n > 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('0');
+
+			for (j = 1; j <= n; j++)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				k = i * j;
+
+				if (k <= 9)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+				else if (k <= 99 && k >= 10)
+				{
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+				}
+				else
+				{
+					l = k / 10;
+					_putchar((l / 10) + '0');
+					_putchar((l % 10) + '0');
+				}
+
+				_putchar((k % 10) + '0');
+			}
+
+			_putchar('\n');
+		}
+	}
 }
