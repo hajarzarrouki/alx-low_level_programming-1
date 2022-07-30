c2ae168d255239c57fe06ac00aa0f378fff5f867
Mubarek Seid <mubareksd@gmail.com>
2022-07-30 21:27:31 +0300

    main header

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,15 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 /*
  * File: main.h
  *
  * Desc: header file
- */
+**/
+
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
-int _putchar(char c);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
@@ -18,4 +20,4 @@ void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
 
-#endif
+#endif /* MAIN_H */
