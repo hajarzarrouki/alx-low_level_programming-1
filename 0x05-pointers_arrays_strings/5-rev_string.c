b3a0745e1d103996f1d37c4ae81ef324c0ead4f2
Mubarek Seid <mubareksd@gmail.com>
2022-07-31 14:07:18 +0300

    len

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,10 +8,10 @@
 
 void rev_string(char *s)
 {
-	int i, len = strlen(s) - 1, j = 0;
+	int i, len = strlen(s), j = 0;
 	char temp;
 
-	for (i = len; i >= len / 2; i--)
+	for (i = len - 1; i >= len / 2; i--)
 	{
 		temp = s[i];
 		s[i] = s[j];
