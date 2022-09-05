13389b84ad99bc0ce25054634cdbceeac1c9d557
Mubarek Seid <mubareksd@gmail.com>
2022-09-05 21:27:45 +0300

    t 3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc *.o -fPIC -shared -o libruin.so
-export LD_PRELOAD=$WPD/libruin.so
+wget -P ../ https://github.com/MubarekSD/alx-low_level_programming/raw/main/0x18-dynamic_libraries/libhack.so
+export LD_PRELOAD=../libhack.so
