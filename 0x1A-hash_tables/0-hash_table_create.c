991a6227e37ee36a1293963e92b0022381e53137
Mubarek Seid <mubareksd@gmail.com>
2022-09-22 17:57:23 +0200

    task 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of the array
+ * Return: a pointer to the newly created hash table
+**/
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+
+	if (!size)
+		return (NULL);
+
+	ht = malloc(sizeof(hash_table_t));
+	
+	if (!ht)
+		return (NULL);
+
+	ht->size = size;
+	ht->array = calloc((size_t)ht->size, sizeof(hash_node_t *));
+
+	if (!ht->array)
+		return (NULL);
+
+	return (ht);
+}
