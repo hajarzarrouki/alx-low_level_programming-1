483b33c9a759aef750cd55a7959c0946f03e985e
Mubarek Seid <mubareksd@gmail.com>
2022-09-23 10:40:43 +0300

    task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -14,13 +14,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	ht = calloc(1, sizeof(hash_table_t));
-	
 	if (!ht)
 		return (NULL);
-
 	ht->size = size;
-	ht->array = calloc((size_t)ht->size, sizeof(hash_node_t *));
-
+	ht->array = calloc(size, sizeof(hash_node_t *));
 	if (!ht->array)
 	{
 		free(ht);
