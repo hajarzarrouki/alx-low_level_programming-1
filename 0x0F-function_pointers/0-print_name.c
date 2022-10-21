8d0e0650fd94d17d50b982d2514b90ea4ebad555
Mubarek Seid <mubareksd@gmail.com>
2022-10-21 18:05:19 +0300

    past task

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -7,14 +7,15 @@
  * @f: is pointer
  * Return: Nothing.
 **/
+
 void print_name(char *name, void (*f)(char *))
 {
-if (name == NULL || f == NULL)
-{
-return;
-}
-else
-{
-f(name);
-}
+	if (name == NULL || f == NULL)
+	{
+		return;
+	}
+	else
+	{
+		f(name);
+	}
 }
