d40e9b06f9a95de5445635bc7b4800b5cdbe9860
Mubarek Seid <mubareksd@gmail.com>
2022-11-24 13:01:48 +0300

    redo task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,14 +9,14 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 9; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (j = '0'; j <= '9'; j++)
 		{
 			if (i != j && i < j)
 			{
-				putchar(i + '0');
-				putchar(j + '0');
+				putchar(i);
+				putchar(j);
 
 				if (i != 8 || j != 9)
 				{
