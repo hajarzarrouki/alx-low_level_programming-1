1013328bc7fd866c7749eb41560f8310cb1a34fd
Mubarek Seid <mubareksd@gmail.com>
2022-11-24 16:36:05 +0300

    redo task 3

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,9 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
+int _islower(int c);
 
-#endif
+#endif /* _MAIN_H_ */
